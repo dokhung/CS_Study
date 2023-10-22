4416cb726354899d79dc39fd24bca2f86779fb1d
dokhung <94828988+dokhung@users.noreply.github.com>
2023-10-22 23:25:05 +0900

    20231020_Mission4 Clear

--- NCS_Start_202310/20231020_Mission4/Program.cs
+++ NCS_Start_202310/20231020_Mission4/Program.cs
@@ -8,6 +8,8 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Xml.XPath;
 
 namespace _20231020_Mission4
 {
@@ -28,21 +30,17 @@ namespace _20231020_Mission4
     ////갤럭시탭   ==
     ////======================================================================
 
-    public enum CompanyType
-    {
-        Apple,
-        Samsung,
-        
-        End
-    }
-    struct LoginInfo
-    {
-        public CompanyType companyType;
-        public string ID;
-    }
+    // public enum CompanyType
+    // {
+    //     Apple,
+    //     Samsung,
+    //     
+    //     End
+    // }
 
     public class Company
     {
+        
         
         public Company()
         {
@@ -55,10 +53,127 @@ namespace _20231020_Mission4
 
     public class Apple : Company
     {
+        // public Dictionary<string, int> AppleList = new Dictionary<string, int>();
+        // AppleList.Add("Iphone",850000);
+        // AppleList.Add("Ipad",2050000);
+        // AppleList.Add("Iwatch",500000);
+        // AppleList.Add("Ipensll",150000);
+        private string[] AppleName = new String[4] { "Iphone", "Ipad", "Iwatch", "Ipensel" };
+        private int[] ApplePrise = new int[4] { 850000, 2050000, 500000, 150000 };
         
         
+        
+
+        public void Login()
+        {
+            string AppleID = "apple123";
+            string ApplePW = "apple123@";
+            Console.WriteLine("Please Login");
+            Console.WriteLine("Please Input Your ID");
+            string AppleIDCheck = "";
+            string ApplePWCheck = "";
+            AppleIDCheck = Console.ReadLine();
+            
+            if (AppleID == AppleIDCheck)
+            {
+                
+                Console.WriteLine("ID OK");
+                Console.WriteLine("");
+                Console.WriteLine("Please Your PW");
+                ApplePWCheck = Console.ReadLine();
+                if (ApplePWCheck == ApplePW)
+                {
+                    Console.WriteLine("PW OK");
+                    Console.WriteLine("Login Start");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    AppleAccess();
+                }
+                else
+                {
+                    Console.WriteLine("The PW is wrong");
+                }
+            }
+            else
+            {
+                Console.WriteLine("The ID is wrong");
+            }
+        }
+        
+        
+
+        public void AppleAccess()
+        {
+            // AppleList.Add("Iphone",850000);
+            // AppleList.Add("Ipad",2050000);
+            // AppleList.Add("Iwatch",500000);
+            // AppleList.Add("Ipensll",150000);
+            Console.WriteLine("This is a list of devices you are selling.");
+            Console.WriteLine("Select one of the following points");
+            // foreach (var item in AppleList)
+            // {
+            //     Console.WriteLine($"기기 : {item.Key}: {item.Value}원");
+            // }
+            // Console.WriteLine();
+            Console.WriteLine("************************************************");
+            for (int i = 0; i < AppleName.Length; i++)
+            {
+                Console.WriteLine($"이름 : {AppleName[i]}:{ApplePrise[i]}원");
+            }
+            Console.WriteLine("************************************************");
+            int SeletedNum = int.Parse(Console.ReadLine());
+            string PlaySelect1 = "";
+            int PlaySelect2 = 0;
+            if (SeletedNum == 1)
+            {
+                Console.WriteLine($"{AppleName[0]}이 선택되었습니다. 가격은 {ApplePrise[0]}원 입니다.");
+                PlaySelect1 = AppleName[0];
+                PlaySelect2 = ApplePrise[0];
+                IPhone iPhone = new IPhone();
+                iPhone.OnNet();
+                iPhone.OnPlay();
+
+            }
+            else if (SeletedNum == 2)
+            {
+                Console.WriteLine($"{AppleName[1]}이 선택되었습니다. 가격은 {ApplePrise[1]}원 입니다.");
+                PlaySelect1 = AppleName[1];
+                PlaySelect2 = ApplePrise[1];
+            }
+            else if (SeletedNum == 3)
+            {
+                Console.WriteLine($"{AppleName[2]}이 선택되었습니다. 가격은 {ApplePrise[1]}원 입니다.");
+                PlaySelect1 = AppleName[2];
+                PlaySelect2 = ApplePrise[2];
+            }
+            else if (SeletedNum == 4)
+            {
+                Console.WriteLine($"{AppleName[3]}이 선택되었습니다. 가격은 {ApplePrise[3]}원 입니다.");
+                PlaySelect1 = AppleName[3];
+                PlaySelect2 = ApplePrise[3];
+            }
+            
+            
+        }
     }
-    
+
+    interface IIPhonePlay
+    {
+        void OnPlay();
+        void OnNet();
+    }
+    class IPhone : Apple,IIPhonePlay
+    {
+        public void OnPlay()
+        {
+            Console.WriteLine("사용되었습니다.");
+        }
+
+        public void OnNet()
+        {
+            Console.WriteLine("사파리 인터넷을 사용합니다.");
+        }
+    }
     
 
     
@@ -68,8 +183,31 @@ namespace _20231020_Mission4
         {
             Console.WriteLine("Please select the company you want");
             Console.WriteLine("One device from the selected enterprise is randomly created. ");
-            Console.Write("1. Apple  /  2. Samsung");
-            Console.WriteLine("");
+            Console.WriteLine("1. Apple  /  2. Samsung");
+            int seletcompany = 0;
+            seletcompany = int.Parse(Console.ReadLine());
+            switch (seletcompany)
+            {
+               case 1:
+                   Console.WriteLine("1. Apple");
+                   //  해당 기업 호출
+                   Apple apple = new Apple();
+                   // 로그인 요구
+                   apple.Login();
+                   // 해당 기업의 물건중 하나 선택할 수 있도록 해야됨
+                   break;
+               case 2:
+                   Console.WriteLine("2. Samsung");
+                   //  해당 기업 호출
+                   // 해당 기업의 물건중 하나 선택할 수 있도록 해야됨
+                   break;
+               default:
+                   Console.WriteLine("Invalid input");
+                   return;
+            }
+            
+            
+            
             
 
         }
