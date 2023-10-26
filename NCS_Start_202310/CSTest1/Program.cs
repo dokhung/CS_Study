06a251200025734c0d9eb611f2dfde649f9ffbe0
dokhung <ehhiop123@naver.com>
2023-10-26 22:50:07 +0900

    redonly

---
+++ NCS_Start_202310/CSTest1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSTest1
+{
+    struct ACSetting
+    {
+        public double currentInCelsius;
+        public double target;
+
+        public  double GetFahrenheit()
+        {
+            target = currentInCelsius * 1.8 + 32;
+            return target;
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ACSetting acs;
+            acs.currentInCelsius = 25;
+            acs.target = 25;
+
+            Console.WriteLine($"{acs.GetFahrenheit()}");
+            Console.WriteLine(acs.target);
+        }
+    }
+}
