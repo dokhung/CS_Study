842639319565f59969bc9e4e2a9d1aa4e68300bd
dokhung <ehhiop123@naver.com>
2023-10-27 15:52:56 +0900

    20231027AM

---
+++ NCS_Start_202310/20231027_suuu/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * sin A = a / b
+ * cos A = c / b
+ * tan A = a / c
+ *
+ *    슬래쉬는 나누기임
+ *
+ *  @ 해석
+ *  sin = 빗변/높이 중 하나를 찾느다면
+ * 곱한다.
+ * 다른것도 동일
+ *
+ *          높이
+ *  sin@ = ------
+ *          빗변
+ *
+ *  의 경우 예를 들면 빗변을 구한다면
+ *  sin x 높이 = 빗변
+ *  이렇게 하면 됨
+ */
+
+namespace _20231027_suuu
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
