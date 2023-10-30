41e85c676026fd7c8ce86d053b2c004df8969a70
dokhung <ehhiop123@naver.com>
2023-10-30 09:53:35 +0900

    ㅁㄴㅇㄹ

---
+++ NCS_Start_202310/NameCard/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace NameCard
+{
+    public class Card
+    {
+        private string name;
+        private string phoneNumber;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
