bd011052cfc39cfe0ab4718f9de660012e202bb7
dokhung <94828988+dokhung@users.noreply.github.com>
2023-11-03 20:45:00 +0900

    pokemon

---
+++ NCS_Start_202310/20231103/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NCS_Start_202310
+{
+    //1번부터 5번 문제는 주석처리 후에 입력해주시고
+    //1번) 삼각함수를 이용하여 x의 길이를 구하는 식을 써주세요
+
+    //cos b = x / a;
+    // x = cos b * a; == 정답
+
+    //2번) 사람이 목적지를 향해 간다고 할 때, 그 사람의 방향벡터를 구하는 식을 써주세요.    
+    //(목적지 - 사람위치) == 정답
+
+    //3번) 다음 문장의 괄호 안을 채우시오.
+    //- 속력과 시간이 주어졌을 때, 속력 (    곱하기    ) 시간  =   (    거리     ) 를 구할 수 있다.
+    //- 스택은 먼저 들어간 것이 (  나중에  ) 나오고, 큐는 먼저 들어간 것이  (  먼저   ) 나온다.
+
+    //4번 ) 마찰이 없는 직선상에서 질량이 2kg인 물체 A는 오른쪽으로 4m/s의 속력으로 움직이고,
+    //질량이 4kg인 물체 B는 왼쪽으로 3m/s의 속력으로 움직이다가 정면충돌하였다.
+    //충돌 후 A가 왼쪽으로 4m/s의 속력으로 움직일때, 충돌후 B의 운동방향과 속력은 몇 m/s인가?
+    // 2kg * 4m/s  + 4kg * (- 3m/s ) = 2kg * (-4m/s) + 4kg * X
+    // 8+ (-12) = -8 + 4X
+    // -4 = -8 + 4X
+    // -4 +8 = -8 +8 +4X
+    // 4 = 4X
+    //X == 1 => B가 오른쪽으로 1m/s 의 속력으로 움직이게 됐음.
+
+    //5번 ) GameManager라는 클래스를 싱글톤으로 만드려고 할 때, 싱글톤이기 위해 반드시 필요한 변수를 선언하세요
+    //static GameManager instance = null;
+    //public static GameManager 인스턴스=>instance; //접근가능...
+
+
+    //6번문제는 코드로 구현하면 됩니다. //필수조건 == 컬렉션을 사용할 것..클래스 최소 1개이상
+    //당신은 주유소 주인입니다. 기름 종류는 휘발유와 경유 두가지 입니다. (gasoline / diesel)
+    //휘발유는 리터당 1800원, 경유는 1600원입니다.
+    //차는 랜덤으로 들어옵니다.
+    //차는 기름 종류와, 필요한 기름양의 정보(최소1.0리터~ 10.0리터 == 소수점 한자리 ),
+    //결제수단 정보를 가지고 있습니다.결제 수단은 현금과 카드입니다.
+    //랜덤으로 차가 10대 들어올 예정이고, 각 차의 필요한 기름만큼 풀로 채워줄 예정입니다.
+    //현금으로 계산할 경우 현금계산입니다 라는 출력을 해주고, 카드일 경우 카드계산이라는 출력을 해줍니다.
+    //영업이 끝나면 (10대의 차에 모든 기름을 넣어주고 나면) , 
+    //현금으로 총 얼마 벌었고, 카드로는 얼마 벌었는지,
+    //그리고 그 두 금액을 합한 총액과 휘발유는 얼마나 나갔고 경유는 얼마나 나갔는지 바로 알 수 있도록 구현해주세요.=>함수..
+    public enum Oil
+    {
+        Gasoline,
+        Diesel,        
+
+        End
+    }
+
+    public enum Pay
+    {
+        Card,
+        Cash,
+
+        End
+    }
+    public class GasStation
+    {
+        int cardval = 0; //카드계산한 값
+        int cashval =0;   //현금계산값
+        float gasol = 0; //총 팔은 가솔린양
+        float diesel = 0; //총 팔은 디젤양
+
+        public Dictionary<Oil, int> oilprice = new Dictionary<Oil, int>(2) { { Oil.Gasoline, 1800}, { Oil.Diesel, 1600 } };
+
+        //1번 모든 차의 정보들을 들고 있다가.. 
+
+        public void AddCar(Car car)
+        {
+            if (car.oil == Oil.Gasoline) //가솔린일떄
+            {
+                gasol += car.oilValue;
+            }
+            else //경유일때
+            {
+                diesel += car.oilValue;
+            }
+            if (car.pay == Pay.Card)
+            {
+                cardval += (int)(car.oilValue * oilprice[car.oil]); //
+            }
+            else 
+            {
+                cashval += (int)(car.oilValue * oilprice[car.oil]); //
+            }            
+        }
+        public int GetMoney(Pay pay) //pay. card 
+        {
+            switch (pay)
+            {
+                case Pay.Card:
+                    return cardval; 
+                    break;
+                case Pay.Cash:
+                    return cashval;
+                    break;                
+                default:
+                    return cardval + cashval;
+                    break;
+            }
+        }
+    }
+
+   
+    public class Car 
+    {        
+        public Oil oil { get; private set; } //내가 쓰는 오일 종류
+        public Pay pay { get; private set; } //결제 방식
+
+        public float oilValue { get; private set; } = 0; //필요 기름양        
+
+        public Car(Random random)
+        {
+            oil = (Oil)random.Next(0, 2);
+            pay = (Pay)random.Next(0, 2);
+            oilValue = random.Next(10, 101 ) * 0.1f; //
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] a)
+        {
+            Random random = new Random();
+            GasStation Me = new GasStation();
+            Car car=null;
+            for (int i = 0; i < 10; i++)
+            {
+                car = new Car(random);
+                Me.AddCar(car);
+            }
+            //카드의 경우
+            Console.WriteLine( Me.GetMoney(Pay.Card));
+
+            Console.WriteLine(Me.GetMoney(Pay.Cash));
+
+            Console.WriteLine(Me.GetMoney(Pay.End));
+        }
+    }
+}
---
+++ NCS_Start_202310/Pokemon/Program.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NCS_Start_202310
+{
+    #region 실습
+
+    //당신에겐 포켓몬 세마리가 있습니다. 각각 풀,불, 물 타입입니다.
+    //풀은 물에게 강하고, 물은 불에게 강하고, 불은 풀에게 강합니다.
+    //각 포켓몬의 피는 20이상 100이하의 랜덤이고, 
+    //각 포켓몬의 공격력은 1이상 10이하입니다.
+
+    //같은 성질끼리 싸우면 본래 가진 공격력으로 공격하고,
+    //내가 상대방에게 지는 성질이면 나의 공격력은 절반이되고, 상대방의 공격력은 내게 공격할 때 두배가 됩니다.
+
+    //포켓몬의 이름은 사용자가 입력하여 지어줍니다. (후에 포켓몬 선택을 이 이름으로 합니다)
+
+    //적으로 다섯마리의 포켓몬을 연달아 만나며, 각 포켓몬 또한 속성이 있습니다. (속성은 랜덤)
+
+    //전투가 시작되면 포켓몬을 선택하여 싸우게 할 수 있으나, 피가 0이하인 포켓몬은 선택할 수 없습니다.
+    //즉, 이전 전투에서 사망시 그 이후 전투에서 그 포켓몬은 더이상 전투 불가
+
+    //전투중 사망시, 남은 포켓몬 중에 골라 진행해야하며, 나의 포켓몬이 전혀 없을 경우 패배가 선언되고 게임이 아예 종료됩니다.
+
+    //전투에서 승리시, 남은 포켓몬들의 정보를 전부 출력합니다.
+
+    public enum Type
+    {
+        GRASS,
+        FIRE,
+        WATER,
+
+        END
+    }
+
+    public class Pokemon
+    {
+        public Type pokeType { get; private set; }
+        public string name { get; private set; } = "";
+        int hp = 0;
+        int maxHP = 0;
+        int att = 0;
+        public bool IsAlive => hp > 0;
+        public int Att => att;
+        Random random = new Random();
+
+        public Pokemon(string name, Type _type = Type.END)
+        {
+            this.name = name;
+            hp = random.Next(20, 101);
+            maxHP = hp;
+            att = random.Next(1, 10);
+
+            if (_type == Type.END)
+            {
+                _type = (Type)random.Next(0, (int)Type.END);
+            }
+            else
+            {
+                pokeType = _type;
+            }
+        }
+        public void Attacked(int damage)
+        {
+            hp -= damage;
+        }
+        public void ShowInfos()
+        {
+            Console.WriteLine("===========================");
+            Console.WriteLine(name);
+            switch (pokeType)
+            {
+                case Type.GRASS:
+                    Console.WriteLine("풀타입 포켓몬");
+                    break;
+                case Type.FIRE:
+                    Console.WriteLine("불타입 포켓몬");
+                    break;
+                case Type.WATER:
+                    Console.WriteLine("물타입 포켓몬");
+                    break;
+            }
+            ShowMyHP();
+            Console.WriteLine($"공격력 : {att}");
+            Console.WriteLine("===========================");
+        }
+        public void ShowMyHP()
+        {
+            Console.WriteLine($"체력 : {hp} / {maxHP}");
+        }
+    }
+
+    public class Master
+    {
+        Dictionary<string, Pokemon> myPokemonDic = new Dictionary<string, Pokemon>(); //내 포켓몬 리스트..
+        Dictionary<Type, List<string>> myPokeTypeDic = new Dictionary<Type, List<string>>(); //원래 의도는 타입별로 빠르게 내가 뭔가 하고싶을까봐..
+        public List<string> aliveList { get; private set; } = new List<string>(); //출전 가능한 리스트...
+
+        public bool SetMyDefaultPokemon(string name, Type _type = Type.END) //나의 포켓몬 등록...
+        {
+            if (myPokemonDic.ContainsKey(name))
+            {
+                Console.WriteLine("동일한 이름으로 지을 수 없습니다");
+                return false;
+            }
+
+            myPokemonDic.Add(name, new Pokemon(name, _type));
+            _type = myPokemonDic[name].pokeType;
+            if (myPokeTypeDic.ContainsKey(_type))
+            {
+                myPokeTypeDic[_type].Add(name);
+            }
+            else
+                myPokeTypeDic.Add(_type, new List<string>() { name });
+
+            aliveList.Add(name);
+
+            return true;
+        }
+
+        public Pokemon GetMyPokemon(string name) //내 포켓몬을 하나 들려서 보내줌..
+        {
+            if (myPokemonDic.ContainsKey(name))
+                return myPokemonDic[name];
+            else
+            {
+                Console.WriteLine("존재하지 않는 포켓몬 입니다.");
+                return null;
+            }
+        }
+        public bool IsAlive(string name)
+        {
+            if (myPokemonDic.ContainsKey(name))
+                return myPokemonDic[name].IsAlive;
+            else
+                return false;
+        }
+
+
+        public void ShowAllMyPokemons()
+        {
+            foreach (var item in myPokemonDic)
+            {
+                item.Value.ShowInfos();
+            }
+        }
+
+        public void DeadPoke(string name)
+        {
+            if (aliveList.Contains(name))
+            {
+                aliveList.Remove(name);
+            }
+        }
+    }
+
+    class Battle
+    {
+        Master Master = null; //나
+        Master Enemy = null; //적
+
+        public void BattleStart()
+        {
+            Master = new Master(); //나ㅓ는 나만의 포켓몬 소유...
+            Enemy = new Master(); //얘는 얘만의 포켓몬 소유
+
+            SettingMasterInfos(); //나와 적의 생성 및 포켓몬 리스트 세팅과, 목록 확인하기 완료
+
+            while (Enemy.aliveList.Count > 0) //상대 포켓몬의 카운트가 사라질때까지 전투.
+            {
+                if (Master.aliveList.Count == 0)
+                {
+                    Console.WriteLine("당신의 패배입니다. 배틀을 종료합니다");
+                    return;
+                }
+
+                Console.WriteLine("배틀을 시작합니다");
+                Console.WriteLine("=========상대 포켓몬==========");
+                Console.Write("타입 : ");
+                string enemyname = Enemy.aliveList[0];
+                Console.WriteLine(Enemy.GetMyPokemon(enemyname)/*Pokemon클래스. 적이 가진 리스트중에 하나 뽑아옴*/.pokeType);
+                Console.WriteLine("출전할 포켓몬을 입력해주세요");
+                //제가 가진 타입별+ 이름 의 나열...
+                string name = Console.ReadLine();
+
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                if (DoBattle(Enemy.GetMyPokemon(Enemy.aliveList[0]), Master.GetMyPokemon(name)))
+                {
+                    Console.WriteLine($"플레이어의 포켓몬{name}이 {enemyname}을 이겼습니다");
+                    Enemy.DeadPoke(enemyname);
+                }
+                else
+                {
+                    Console.WriteLine($"상대 포켓몬{enemyname}이 {name}을 이겼습니다");
+                    Master.DeadPoke(name);
+                }
+            }
+
+            Console.WriteLine("배틀에 승리하셨습니다");
+            Master.ShowAllMyPokemons();
+        }
+
+        bool DoBattle(Pokemon enemypoke, Pokemon playerpoke)
+        {
+            while (true)
+            {
+                Console.WriteLine("플레이어의 공격");
+                Attack(playerpoke, enemypoke);
+                Console.WriteLine();
+
+                Console.Write("적의 ");
+                enemypoke.ShowMyHP();
+                Console.WriteLine();
+                if (enemypoke.IsAlive == false)
+                {
+                    return true;
+                }
+
+                Console.WriteLine("적의 공격");
+                Attack(enemypoke, playerpoke);
+                Console.WriteLine();
+                Console.Write("플레이어의 ");
+                playerpoke.ShowMyHP();
+                Console.WriteLine();
+                if (playerpoke.IsAlive == false)
+                {
+                    return false;
+                }
+            }
+        }
+
+        void Attack(Pokemon attackPoke, Pokemon hitPoke)
+        {
+            int val = (int)(attackPoke.Att * IsWin(attackPoke.pokeType, hitPoke.pokeType)); //val == 최종공격력
+            Console.WriteLine($"{attackPoke.name}의 공격력 : {val} (원래 공격력 : {attackPoke.Att})");
+            hitPoke.Attacked(val);
+        }
+
+        float IsWin(Type type1, Type type2)
+        {
+            switch (type1)
+            {
+                case Type.GRASS:
+                    if (type2 == Type.FIRE)
+                        return 0.5f;
+                    else if (type2 == Type.WATER)
+                        return 2;
+                    break;
+                case Type.FIRE:
+                    if (type2 == Type.WATER)
+                        return 0.5f;
+                    else if (type2 == Type.GRASS)
+                        return 2;
+                    break;
+                case Type.WATER:
+                    if (type2 == Type.GRASS)
+                        return 0.5f;
+                    else if (type2 == Type.FIRE)
+                        return 2;
+                    break;
+            }
+
+            return 1;
+        }
+        void SettingMasterInfos()
+        {
+            string name = "";
+            Console.WriteLine("당신의 포켓몬에게 이름을 정해주세요");
+            Console.Write("풀타입 포켓몬 : ");
+            name = Console.ReadLine();
+            Master.SetMyDefaultPokemon(name, Type.GRASS);
+            Console.Write("불타입 포켓몬 : ");
+            name = Console.ReadLine();
+            Master.SetMyDefaultPokemon(name, Type.FIRE);
+            Console.Write("물타입 포켓몬 : ");
+            name = Console.ReadLine();
+            Master.SetMyDefaultPokemon(name, Type.WATER);
+            //==============================
+
+            Random random = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                Enemy.SetMyDefaultPokemon("포켓몬" + (i + 1), (Type)random.Next(0, (int)Type.END));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("================================");
+            Console.WriteLine("플레이어 포켓몬");
+            Master.ShowAllMyPokemons();
+            Console.WriteLine();
+            Console.WriteLine("################################");
+            Console.WriteLine();
+            Console.WriteLine("적의 포켓몬");
+            Enemy.ShowAllMyPokemons();
+            Console.WriteLine("================================");
+            Console.WriteLine();
+        }
+    }
+
+    //class Program
+    //{
+    //    static void Main(string[] a)
+    //    {
+    //        Battle battle = new Battle();
+    //        battle.BattleStart();
+
+    //    }
+    //}
+    #endregion
+}
