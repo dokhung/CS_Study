1721dd2912efa4b4a0de569d7b164091ba1627b2
呉ドギョン <kuredogyon@kuredogyonui-MacBookea.local>
2023-11-06 00:24:43 +0900

    you

---
+++ NCS_Start_202310/ramdafun/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ramdafun
+{
+    class MyClass
+    {
+        public string fuck(string you)
+        {
+            you = "nininin";
+            return you = "ninin";
+        }
+        
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            string you = "";
+            myClass.fuck(you);
+        }
+    }
+}
